1b37dd13117ee33d5098ed4d12af0e5b5da2e236
Mateusz Kumorek <spzeg4mk@gmail.com>
2026-06-13 19:52:30 +0200

    Wstępna implementacja

--- lib/models/pokoj.dart
+++ lib/models/pokoj.dart
@@ -1,3 +1,47 @@
+import '../enums/status_pokoju.dart';
+import '../enums/status_rezerwacji.dart';
+import 'rezerwacja.dart';
+
 class Pokoj {
-  
+  Pokoj({
+    required this.idPokoju,
+    required this.nrPokoju,
+    required this.pojemnoscPokoju,
+    required this.cenaZaDobe,
+    required this.statusPokoju,
+    List<Rezerwacja>? rezerwacje,
+  }) : rezerwacje = rezerwacje ?? [];
+
+  final int idPokoju;
+  final int nrPokoju;
+  final int pojemnoscPokoju;
+  double cenaZaDobe;
+  StatusPokoju statusPokoju;
+  final List<Rezerwacja> rezerwacje;
+
+  double obliczKoszt(int iloscDni) {
+     throw NotImplementedError('Metoda nie została jeszcze zaimplementowana');
+  }
+
+  void zmienStatus(StatusPokoju nowyStatus) {
+     throw NotImplementedError('Metoda nie została jeszcze zaimplementowana');
+  }
+
+  bool czyDostepny(DateTime dataPoczatkowa, DateTime dataKoncowa) {
+     throw NotImplementedError('Metoda nie została jeszcze zaimplementowana');
+    }
+
+
+  bool _blokujeDostepnosc(Rezerwacja rezerwacja) {
+    throw NotImplementedError('Metoda nie została jeszcze zaimplementowana');
+  }
+
+  bool _terminySiePokrywaja(
+    DateTime poczatekA,
+    DateTime koniecA,
+    DateTime poczatekB,
+    DateTime koniecB,
+  ) {
+     throw NotImplementedError('Metoda nie została jeszcze zaimplementowana');
+  }
 }
